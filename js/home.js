75aa8ac1b2ea0d1a7e752d8e72499f2eaab4d337
Shahriar Ratul <shahriarratul01921@gmail.com>
2024-09-19 16:32:31 +0600

    added add money section

--- js/home.js
+++ js/home.js
@@ -1,1 +1,11 @@
-console.log('added js file')
+document.getElementById('add-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const pinNumber =document.getElementById('pin-btn').value;
+    if(pinNumber === '1234'){
+        const addAmount = document.getElementById('add-amount').value;
+        const currentAmount = document.getElementById('current-amount').innerText;
+        const totalAmount = parseInt(addAmount)+ parseInt(currentAmount);
+        console.log(totalAmount)
+        
+    }
+});
