846a78c28e8975ce673871ff70a2c074584de004
Shahriar Ratul <shahriarratul01921@gmail.com>
2024-09-19 17:25:19 +0600

    cash out section complete

--- js/home.js
+++ js/home.js
@@ -6,6 +6,23 @@ document.getElementById('add-btn').addEventListener('click', function(event){
         const currentAmount = document.getElementById('current-amount').innerText;
         const totalAmount = parseInt(addAmount)+ parseInt(currentAmount);
         console.log(totalAmount)
-        
+        document.getElementById('current-amount').innerText = totalAmount;
+    }
+    else{
+        alert('Failed to add money! please try again. ')
     }
 });
+
+document.getElementById('cashOut-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const inputAmount = document.getElementById('input-amount').value;
+    const inputPin = document.getElementById('input-pin').value;
+    if(inputPin === '1234'){
+        const currentAmount = document.getElementById('current-amount').innerText;
+        const totalAmount = parseFloat(currentAmount) - parseFloat(inputAmount);
+        document.getElementById('current-amount').innerText = totalAmount;
+    }
+    else{
+        alert('Failed to add money! please try again.')
+    }
+})
