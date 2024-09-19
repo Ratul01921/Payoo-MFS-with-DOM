5a8df688663bda0dfd6083d1a173793b34c9de6c
Shahriar Ratul <shahriarratul01921@gmail.com>
2024-09-19 15:56:33 +0600

    added home section

--- js/login.js
+++ js/login.js
@@ -1,20 +1,21 @@
 document.getElementById('btn-login').addEventListener('click', function(event){
    event.preventDefault();
-    console.log('login button clicked')
+    // console.log('login button clicked')
 
     const phoneNumber =  document.getElementById('phone-number');
     const phnNum = phoneNumber.value;
-    console.log(phnNum);
+    // console.log(phnNum);
 
     const passwordNumber = document.getElementById('password');
     const passNum = passwordNumber.value;
-    console.log(passNum);
+    // console.log(passNum);
 
     if(phnNum === '01921671587' && passNum === '1234'){
-        console.log('you are eligible this website')
+        console.log('you are logged in');
+        window.location.href = './home.html'
     }
     else{
-        console.log('you are not eligible this website')
+        alert('wrong phone number or password')
     }
 
 })
